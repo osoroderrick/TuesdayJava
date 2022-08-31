0de0a97360b05ce8deb6346288a770a013eb1144
osoroderrick <osoroderrick@yahoo.com>
2022-08-30 22:29:36 -0400

    finished product

--- src/main/java/TuesdayJavaHomework.java
+++ src/main/java/TuesdayJavaHomework.java
@@ -1,13 +1,18 @@
 public class TuesdayJavaHomework {
     public static void main(String[] args) {
-        // Java Home (print "Hello World"
+        // Java Syntax
         System.out.println("Hello World");
+
+        //Multi-Line Comments
+        /* This is and example
+        of what a multi line comment looks like. */
 
         //Variables
 
         //String
         String name = "Derrick";
         System.out.println(name);
+        System.out.println("Hello" + name);
 
         //int
         int number = 21;
@@ -36,6 +41,29 @@ public class TuesdayJavaHomework {
         //long
         long numb3 = 789456123L;
         System.out.println(numb3);
+
+        //double
+        double numb4 = 22.22d;
+        System.out.println(numb4);
+
+        //Display Variables
+        System.out.println("Hello " + name ); //String
+
+        //Declare Multiple Variables
+        int x = 5;
+        int y = 6;
+        int z = 7;
+        System.out.println(x + y + z);  // integer
+
+        //One Value Multiple Variable
+        int a, b, c;
+        a = b = c = 99;
+        System.out.println(a + b + c);
+
+
+
+
+
 
 
 
