9153a63fd8932e74ddfc6ab89e5fb709c00068ec
osoroderrick <osoroderrick@yahoo.com>
2022-08-30 22:02:34 -0400

    more progress

--- src/main/java/TuesdayJavaHomework.java
+++ src/main/java/TuesdayJavaHomework.java
@@ -4,14 +4,41 @@ public class TuesdayJavaHomework {
         System.out.println("Hello World");
 
         //Variables
+
+        //String
         String name = "Derrick";
         System.out.println(name);
 
+        //int
         int number = 21;
         System.out.println(number);
 
+        //float
         float f1 = 3.14159f;
         System.out.println(f1);
+
+        //boolean
+        boolean bool1 = true;
+        System.out.println(bool1);
+
+        //char
+        char letter1 = 'A';
+        System.out.println(letter1);
+
+        //byte
+        byte numb1 = 112;
+        System.out.println(numb1);
+
+        //short
+        short numb2 = 4000;
+        System.out.println(numb2);
+
+        //long
+        long numb3 = 789456123L;
+        System.out.println(numb3);
+
+
+
     }
 
             }
