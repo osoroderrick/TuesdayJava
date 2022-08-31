821969af842eb5fe874dd9f7f517c2bb6f47187e
osoroderrick <osoroderrick@yahoo.com>
2022-08-30 21:46:19 -0400

    Tuesday Java Homework

---
+++ src/main/java/TuesdayJavaHomework.java
@@ -0,0 +1,17 @@
+public class TuesdayJavaHomework {
+    public static void main(String[] args) {
+        // Java Home (print "Hello World"
+        System.out.println("Hello World");
+
+        //Variables
+        String name = "Derrick";
+        System.out.println(name);
+
+        int number = 21;
+        System.out.println(number);
+
+        float f1 = 3.14159f;
+        System.out.println(f1);
+    }
+
+            }
